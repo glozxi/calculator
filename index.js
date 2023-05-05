5ae9b182e56b2cb5e111c97ed9c008ac47350300
glozxi <glozxi@gmail.com>
2023-05-05 11:01:18 +0800

    Add displaying numbers inputted

--- index.js
+++ index.js
@@ -14,10 +14,6 @@ function divide(a, b) {
     return a / b;
 }
 
-let numberA;
-let operator;
-let numberB;
-
 function operate(a, op, b) {
     switch(op) {
         case "+":
@@ -29,4 +25,51 @@ function operate(a, op, b) {
         case "/":
             return divide(a, b);
     }
+}
+
+function reset() {
+    numberA = "";
+    operator = "";
+    numberB = "";
+    operationDisplay.textContent = "";
+    resultDisplay.textContent = "";
+}
+
+function getResult() {
+    if (numberA === "" | operator === "" || numberB === "") {
+        return;
+    }
+
+    const res = operate(Number(numberA), operator, Number(numberB));
+    resultDisplay.textContent = res;
+    numberA = res.toString();
+    operator = "";
+    numberB = "";
+    operationDisplay.textContent = numberA;
+}
+
+function keyInNumber(num) {
+    if (operator === "") {
+        numberA += num;
+        operationDisplay.textContent += num;
+    } else {
+        numberB += num;
+        operationDisplay.textContent += num;
+    }
+}
+
+let numberA = "";
+let operator = "";
+let numberB = "";
+
+const clearButton = document.getElementById("clear").addEventListener('click', reset);
+const equalButton = document.getElementById("equal").addEventListener('click', getResult);
+
+const operationDisplay = document.getElementById("operation");
+const resultDisplay = document.getElementById("result");
+
+const numberButtons = document.querySelectorAll("#number-buttons > button");
+for (let button of numberButtons) {
+    let numInButton = button.textContent;
+    button.addEventListener('click', () => keyInNumber(numInButton));
 }
